492e2dd59f01c9bf180aa41a6e056e67b58fae39
Domas Trijonis <domas.trijonis@gmail.com>
2025-03-08 18:47:27 +0100

    Fix used methods lookup to not break with extra whitespace

--- src/helpers/get-used-methods.ts
+++ src/helpers/get-used-methods.ts
@@ -1,7 +1,7 @@
 
 export function getUsedMethods(sourceCode: string, variable: string) {
   const result: string[] = [];
-  const regex = new RegExp(`${variable}\\\.([a-zA-Z0-9]+)[\\\(<]`, 'g');
+  const regex = new RegExp(`${variable}[\\\s]*\\\.([a-zA-Z0-9]+)[\\\(<]`, 'g');
   let matches: RegExpExecArray | null;
 
   while (matches = regex.exec(sourceCode)) {
